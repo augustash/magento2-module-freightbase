76e118c184ea7aca5ceeb61372dbd6674418fe54
kazajhodo <acornell@augustash.com>
2018-05-25 11:00:46 -0500

    Updating javascript overrides
    Learned how to properly override magento javascript.

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -1,14 +1,16 @@
 var config = {
     map: {
         '*': {
-        	'Magento_Checkout/template/shipping.html': 'Augustash_FreightBase/template/shipping.html',
-        	'Magento_Checkout/js/view/shipping': 'Augustash_FreightBase/js/view/shipping'
+        	'Magento_Checkout/template/shipping.html': 'Augustash_FreightBase/template/shipping.html'
         }
   	},
   	config: {
         mixins: {
             'Magento_Checkout/js/action/select-shipping-address': {
                 'Augustash_FreightBase/js/action/select-shipping-address-mixin': true
+            },
+            'Magento_Checkout/js/view/shipping': {
+                'Augustash_FreightBase/js/view/shipping-mixin': true
             }
         }
     }
